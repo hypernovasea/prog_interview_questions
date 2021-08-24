da4bb5ca166e99241d1eaeee83b4eeea90a7f8e9
Artemis <fs031090@gmail.com>
2021-08-24 12:20:40 -0400

    added more meaningful var names; used better data structure

--- LongestAbsoluteFilePath.java
+++ LongestAbsoluteFilePath.java
@@ -1,33 +1,45 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
 
+import java.util.Stack;
+
+/**
+ *  Longest Absolute File Path
+ *  https://leetcode.com/problems/longest-absolute-file-path/
+ * 
+ *  level: medium
+ *  ans: 
+ */
 public class LongestAbsoluteFilePath {
 
 	public static void main(String[] args) {
-		String s = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
-		System.out.println( lengthLongestPath(s) );
+
+        String path1 = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext";
+        System.out.println(lengthLongestPath(path1));
+
+		String path2 = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+		System.out.println(lengthLongestPath(path2));
 	}
 
 	
-	
-	public static int lengthLongestPath(String input){
-		Deque<Integer> stack = new ArrayDeque<Integer>();
+	public static int lengthLongestPath(String input) {
+		Stack<Integer> stack = new Stack<Integer>();
         stack.push(0); // "dummy" length
         int maxLen = 0;
-        for(String s:input.split("\n")){
+
+        // break string by new line since that indicates a new file
+        for (String file: input.split("\n")) {
         	
-        	System.out.println(s);
-        	
-            int lev = s.lastIndexOf("\t")+1; // number of "\t"
+        	System.out.println(file);
+            int level = file.lastIndexOf("\t")+1; // level == number of "\t"
             
-            while( lev+1 < stack.size() ) 
+            while (level+1 < stack.size()) 
             	stack.pop(); // find parent
             
-            int len = stack.peek() + s.length()-lev+1; // remove "/t", add"/"
+            int len = stack.peek() + (file.length() - level + 1); // remove "/t", add"/"
             stack.push(len);
             
-            // check if it is file
-            if(s.contains(".")) maxLen = Math.max(maxLen, len-1); 
+            // check if it is a file
+            if (file.contains(".")) 
+                maxLen = Math.max(maxLen, len-1); 
         }
         return maxLen;
 	} 
