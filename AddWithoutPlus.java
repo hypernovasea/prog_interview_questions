7002fe55e7679dc3a68c2e63e3e8d8a3e9fc8da9
Artemis <fs031090@gmail.com>
2021-08-28 13:03:54 -0400

    added add without plus question

---
+++ AddWithoutPlus.java
@@ -0,0 +1,27 @@
+
+//Add two numbers without using the + operator. 
+
+/**
+ *  Add Without Plus
+ * 
+ *  level: easy
+ *  ans: use binary addition
+ */
+public class AddWithoutPlus {
+
+	public static void main(String[] args) {
+		addWithoutPlus(8, 76); // should print out '84'
+	}
+	
+	
+	public static void addWithoutPlus(int a, int b) {
+		while(b != 0) {
+			int carry = a & b;	//calculate the carry bit
+			a = a ^ b;			//calculate the sum
+			b = carry << 1;		//shifts carry to 1 bit to calculate sum
+		}
+		
+		System.out.println(a);
+	}
+
+}
